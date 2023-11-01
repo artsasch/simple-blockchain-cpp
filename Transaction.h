c515becf06d6321b750ea71dc1206c5c41389d94
sashh <sashheka@gmail.com>
2023-11-01 23:20:18 +0300

    feat: add Transaction class in Transaction.h

---
+++ Transaction.h
@@ -0,0 +1,29 @@
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+
+#include <string>
+#include <openssl/rsa.h>
+
+class Transaction {
+private:
+  std::string senderAddress;
+  std::string recipientAddress;
+  double amount;
+  std::string signature;
+
+public:
+  Transaction(std::string sender, std::string recipient, double amt);
+
+  std::string calculateHash() const;
+
+  void signTransaction(RSA* privateKey);
+
+  bool verifyTransaction() const;
+
+  std::string getSenderAddress() const;
+  std::string getRecipientAddress() const;
+  double getAmount() const;
+  std::string getSignature() const;
+};
+
+#endif
