0400fe966b4b63fc3893a40eac06614819bb1704
sashh <sashheka@gmail.com>
2023-11-20 13:46:45 +0300

    test(main.cpp): tested work of Block, Wallet, Transaction and Blockchain classes

--- main.cpp
+++ main.cpp
@@ -1,28 +1,34 @@
-#include "Block.h"
 #include "Transaction.h"
+#include "Wallet.h"
+#include "Block.h"
+#include "Blockchain.h"
 #include <iostream>
-#include <Windows.h>
+#include <string>
 
 int main() {
-    Transaction t1("sender1", "recipient1", 25.0, "hello) buy some shoes");
-    Transaction t2("sender2", "recipient2", 35.0, "buy me burger pls");
-    Transaction t3("sender3", "recipient3", 15.0, "for your work");
+    Blockchain* chain1 = Blockchain::getInstance();
+    Blockchain* chain2 = Blockchain::getInstance();
+    Wallet w(100.0);
 
-    Block b1({t1, t2, t3}, "0");
+    std::string privateKey = w.getPrivateKey();
+    std::string publicKey = w.getPublicKey();
+    std::string address = w.getAddress();
+    double balance = w.getBalance();
 
-    Sleep(1000);
+    std::cout << "Wallet privateKey: " << privateKey << std::endl;
+    std::cout << "Wallet publicKey: " << publicKey << std::endl;
+    std::cout << "Wallet address: " << address << std::endl;
+    std::cout << "Wallet balance: " << balance << std::endl;
 
-    Transaction t4("sender1", "recipient1", 25.0, "hello) buy some shoes");
-    Transaction t5("sender2", "recipient2", 35.0, "buy me burger pls");
-    Transaction t6("sender3", "recipient3", 15.0, "for your work");
+    Transaction t1{"sender1", "recipient1", 15.0, "transaction1)"};
+    Transaction t2{"sender2", "recipient2", 25.0, "transaction2)"};
 
-    Block b2({t4, t5, t6}, b1.getHash());
+    Block b;
+    b.addTransaction(t1);
+    b.addTransaction(t2);
+    b.printBlockTransactions();
 
-    b1.printBlock();
-    std::cout << "b1 Block transactions" << std::endl;
-    b1.printBlockTransactions();
-    std::cout << "--------------------------" << std::endl;
-    b2.printBlock();
-    std::cout << "b2 Block transactions" << std::endl;
-    b2.printBlockTransactions();
+    chain1->addBlock(b);
+    chain2->addBlock(b);
+    chain1->printBlockchainBlocks();
 }
