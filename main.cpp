644411ef0ca92616e773c6017d2136af96e49a0d
sashh <sashheka@gmail.com>
2023-11-29 19:32:22 +0300

    abib)

--- main.cpp
+++ main.cpp
@@ -14,89 +14,47 @@ std::vector<std::string> split(const std::string& str) {
 }
 
 int main() {
-    Blockchain* chain = Blockchain::getInstance();
-
     std::string input;
+    Blockchain* chain = Blockchain::getInstance();
     Wallet* w = nullptr;
     Block* b = nullptr;
     Transaction* t = nullptr;
-    std::string prevBlockHash;
 
     while (true) {
       std::cout << "> ";
       std::getline(std::cin, input);
       std::vector<std::string> commands = split(input);
 
-      if (commands.empty()) continue;
+      if (commands.empty())
+        continue;
 
-      if (commands[0] == "exit") {
+      if (commands[0] == "exit")
         break;
-      }
 
-      if (commands[0] == "createwallet") {
+      if (commands[0] == "cw")
         handleCreateWallet(w, commands);
-      }
 
-      if (commands[0] == "printwallet") {
+      if (commands[0] == "pw")
         handlePrintWallet(w);
-      }
 
-      if (commands[0] == "createblock") {
-        if (b != nullptr) {
-          prevBlockHash = b->getHash();
-          delete b;
-          b = new Block(prevBlockHash);
-        } else {
-          delete b;
-          b = new Block();
-        }
-        std::cout << "Block created." << std::endl;
-      }
+      if (commands[0] == "cb")
+        createBlock(b);
 
-      if (commands[0] == "createtr") {
-        delete t;
-        std::string transactionSender = commands[1];
-        std::string transactionRecipient = commands[2];
-        int transactionAmount = std::stoi(commands[3]);
-        std::string transactionData = commands[4];
-        t = new Transaction(transactionSender, transactionRecipient, transactionAmount, transactionData);
-        b->addTransaction(*t);
-        std::cout << "Transaction created and added to block." << std::endl;
-      }
+      if (commands[0] == "ct")
+        createTransaction(b, t, commands);
 
-      if (commands[0] == "printblocktrs") {
-        b->printBlockTransactions();
-      }
+      if (commands[0] == "pbt")
+        printBlockTransactions(b);
 
-      if (commands[0] == "addblockinblockchain") {
-        chain->addBlock(*b);
-        std::cout << "Block added in blockchain." << std::endl;
-      }
+      if (commands[0] == "abib")              //haha abib)
+        addBlockInBlockchain(chain, b);
 
-      if (commands[0] == "printchain") {
-        chain->printBlockchainBlocks();
-      }
-
+      if (commands[0] == "pc")
+        printBlockchainBlocks(chain);
     }
+
+    delete chain;
     delete w;
     delete b;
     delete t;
-
-
-//    Wallet w(100.0);
-//
-//    w.printWallet();
-//
-//    Transaction t1{"sender1", "recipient1", 15.0, "transaction1"};
-//    Transaction t2{"sender2", "recipient2", 25.0, "transaction2"};
-//
-//    Block b1;
-//    b1.addTransaction(t1);
-//    chain->addBlock(b1);
-//
-//    Block b2(b1.getHash());
-//    b2.addTransaction(t2);
-//    chain->addBlock(b2);
-//
-//    chain->printBlockchainBlocks();
 }
